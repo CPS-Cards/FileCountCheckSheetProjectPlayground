61329754e7fe41a070624b73ef792ec5bd4bfb76
Erik May <emay@cpscards.com>
2021-09-22 15:55:02 -0400

    Added to logic

--- Program.cs
+++ Program.cs
@@ -8,7 +8,13 @@ namespace FileCountCheckSheetProjectPlayground
         public const string SLAFile = @"S:\Planner\Daily Reporting\Daily reporting data.tab";
         static void Main(string[] args)
         {
-            int jobNumber = 5495;
+            DayTest();
+        }
+
+        public static void DayTest()
+        {
+            Console.WriteLine("Input a Job Number:\n");
+            int jobNumber = Int32.Parse(Console.ReadLine());
             var sla = GetJobSLANumber(jobNumber);
             var day = GetDay(sla);
 
@@ -23,12 +29,16 @@ namespace FileCountCheckSheetProjectPlayground
             for (int i = 1; i < file.Length - 1; i++)
             {
                 var parts = file[i].Split('\t');
-                if (Int32.Parse(parts[0]) == number) return Int32.Parse(parts[3]);
+                if (parts[3] == string.Empty) continue;
+                if (Int32.Parse(parts[0]) == number)
+                    return Int32.Parse(parts[3]);
             }
             return -1;
         }
         public static string GetDay(int offset)
         {
+            if (offset < 0)
+                return "Default";
             var today = DateTime.Now;
             today = today.AddDays(offset);
             var result = today.DayOfWeek.ToString();
